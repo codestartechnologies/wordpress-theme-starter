024c26d74442befa9d8649d33d6e1862b2321ad1
Chijindu G. Nzeako <chijindunzeako517@gmail.com>
2022-11-01 04:06:23 +0100

    added classes for printing admin notices
    - added abstract class AdminNotice for handling add_notice() method
    
    - added class AdminNotice for printing notices to the screen

---
+++ src/Abstracts/AdminNotice.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * AdminNotice abstract class file.
+ *
+ * This file contains AdminNotice abstract class which contains contracts for classes that will register admin notifications.
+ *
+ * @package     WordpressThemeStarter
+ * @author      Chijindu Nzeako <chijindunzeako517@gmail.com>
+ * @link        https://codestar.com.ng
+ * @since       1.0.0
+ */
+
+namespace Codestartechnologies\WordpressThemeStarter\Abstracts;
+
+use Codestartechnologies\WordpressThemeStarter\Interfaces\ActionHooks;
+
+/**
+ * Prevent direct access to this file.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! class_exists( 'AdminNotice' ) ) {
+    /**
+     * Class Customizer
+     *
+     * This class contains contracts that will be used to register customizer sections, settings,
+     * and controls.
+     *
+     * @package WordpressThemeStarter
+     * @author Chijindu Nzeako <chijindunzeako517@gmail.com>
+     */
+    abstract class AdminNotice implements ActionHooks {
+        /**
+         * Register add_action() and remove_action().
+         *
+         * @access public
+         * @return void
+         * @since 1.0.0
+         */
+        public function register_actions(): void
+        {
+            add_action( 'admin_notices', array( $this, 'notification' ) );
+        }
+
+        /**
+         * "admin_notices" action callback
+         *
+         * @access public
+         * @abstract
+         * @return void
+         * @since 1.0.0
+         */
+        abstract public function notification() : void;
+    }
+}
---
+++ src/Admin/Notices/AdminNotice.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * AdminNotice class file.
+ *
+ * This file contains AdminNotice class which contains methods for printing admin notifications to the screen.
+ *
+ * @package     WordpressThemeStarter
+ * @author      Chijindu Nzeako <chijindunzeako517@gmail.com>
+ * @link        https://codestar.com.ng
+ * @since       1.0.0
+ */
+
+namespace Codestartechnologies\WordpressThemeStarter\Admin\Notices;
+
+use Codestartechnologies\WordpressThemeStarter\Abstracts\AdminNotice as AbstractsAdminNotice;
+
+/**
+ * Prevent direct access to this file.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! class_exists( 'AdminNotice' ) ) {
+    /**
+     * Class AdminNotice
+     *
+     * This class contains methods for printing admin notifications to the screen. This class implements one method: notification().
+     *
+     * @package WordpressThemeStarter
+     * @author  Chijindu Nzeako <chijindunzeako517@gmail.com>
+     */
+    final class AdminNotice extends AbstractsAdminNotice {
+        /**
+         * "admin_notices" action callback
+         *
+         * @access public
+         * @return void
+         * @since 1.0.0
+         */
+        public function notification() : void
+        {
+            //
+        }
+    }
+}
