1d247831ba935308c3bae72ead8cfeb3ef7253d2
Chijindu G. Nzeako <chijindunzeako517@gmail.com>
2023-04-06 15:40:32 +0100

    updated autoloading feature
    - created WTSAutoLoader::class in autoload.php for autoloading classes.
    
    - removed autoload settings from composer.json.

--- autoload.php
+++ autoload.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * This file contains an autoloading function for loading required classes.
+ * WTSAutoLoader class file
+ *
+ * This file contains WTSAutoLoader class which will be used to require class files and function files dynamically.
  *
  * @package     WordpressThemeStarter
  * @author      Chijindu Nzeako <chijindunzeako517@gmail.com>
@@ -9,28 +11,109 @@
  * @since       1.0.0
  */
 
-spl_autoload_register( 'wts_autoloader' );
+namespace Codestartechnologies\WordpressThemeStarter;
 
 /**
- * Function to autoload classes starting with WTS_Theme namespace
- *
- * @param string $class
- * @return void
- * @since 1.0.0
+ * Prevent direct access to this file.
  */
-function wts_autoloader( string $class ) : void
-{
-    $namespace = 'WTS_Theme\App';
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 
-    if ( strpos( $class, $namespace ) !== 0 ) {
-        return;
+/**
+ * WTSAutoLoader class
+ *
+ * This class is used to require class files and function files dynamically.
+ *
+ * @package WordpressThemeStarter
+ * @author  Chijindu Nzeako <chijindunzeako517@gmail.com>
+ */
+final class WTSAutoLoader
+{
+    /**
+     * Prefix to add to namespace
+     *
+     * @access protected
+     * @static
+     * @var string|null
+     * @since 1.0.0
+     */
+    protected static ?string $prefix = null;
+
+    /**
+     * Auto load class mappings
+     *
+     * @access protected
+     * @static
+     * @var array
+     * @since 1.0.0
+     */
+    protected static array $class_autoloads = array(
+        "WTS_Theme\\App" => "app",
+        "Codestartechnologies\\WordpressThemeStarter" => "src",
+    );
+
+    /**
+     * Auto load file mappings
+     *
+     * @access protected
+     * @static
+     * @var array
+     * @since 1.0.0
+     */
+    protected static array $file_autoloads = array(
+        "src/Helpers/general.php",
+        "src/Helpers/pagination.php",
+        "src/Helpers/posts.php",
+        "app/functions.php",
+    );
+
+    /**
+     * Registers autoloader for classes and handles autoloading for custom files
+     *
+     * @static
+     * @return void
+     * @since 1.0.0
+     */
+    public static function autoload() : void
+    {
+        spl_autoload_register( array( __CLASS__, 'wts_autoloader' ) );
+
+        $path = get_template_directory() . DIRECTORY_SEPARATOR;
+
+        foreach ( self::$file_autoloads as $file ) {
+            if ( is_readable( $path . $file ) ) {
+                require_once( $path . $file );
+            }
+        }
     }
 
-    $class = str_replace( $namespace, '', $class );
-    $class = str_replace( '\\', DIRECTORY_SEPARATOR, $class ) . '.php';
-    $path = get_template_directory() . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . $class;
+    /**
+     * Handles autoloading for classes.
+     *
+     * @static
+     * @param string $class
+     * @return void
+     * @since 1.0.0
+     */
+    public static function wts_autoloader( string $class ) : void
+    {
+        $prefix = isset( self::$prefix ) ? self::$prefix . "\\" : '';
 
-    if ( is_readable( $path ) ) {
-        require_once( $path );
+        foreach ( self::$class_autoloads as $namespace => $dir ) {
+            $namespace = $prefix . $namespace;
+
+            if ( strpos( $class, $namespace ) === 0 ) {
+                $class = str_replace( $namespace, '', $class );
+                $class = str_replace( '\\', DIRECTORY_SEPARATOR, $class ) . '.php';
+                $path = get_template_directory() . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $class;
+
+                if ( is_readable( $path ) ) {
+                    require_once( $path );
+                }
+            }
+        }
     }
 }
+
+\Codestartechnologies\WordpressThemeStarter\WTSAutoLoader::autoload();
