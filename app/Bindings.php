8f9866fdd3df3495e0d49693fc2ac68704415d02
Chijindu G. Nzeako <chijindunzeako517@gmail.com>
2023-04-09 12:08:25 +0100

    added new comment blocks to Bindings class

--- app/Bindings.php
+++ app/Bindings.php
@@ -2,7 +2,7 @@
 /**
  * Bindings class file.
  *
- * This file contains Bindings class which returns classes that will be registered with the theme.
+ * This file contains Bindings class used to register classes that are needed to add features to the theme.
  *
  * @package    WordpressThemeStarter
  * @author     Chijindu Nzeako <chijindunzeako517@gmail.com>
@@ -13,6 +13,7 @@
 
 namespace WTS_Theme\App;
 
+use WP_Widget_Calendar;
 use WTS_Theme\App\Admin\Customizers\WTSCustomizer;
 use WTS_Theme\App\Admin\Menus\WTSMenuPage;
 use WTS_Theme\App\Admin\Menus\WTSOptionsPage;
@@ -20,6 +21,7 @@ use WTS_Theme\App\Admin\Menus\WTSThemePage;
 use WTS_Theme\App\Admin\Notices\WTSNotice;
 use WTS_Theme\App\Admin\Settings\WTSSettings;
 use WTS_Theme\App\Public\Sidebars\WTSSidebar;
+use WTS_Theme\App\Public\Widgets\WTSWidget;
 
 /**
  * Prevent direct access to this file.
@@ -29,9 +31,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Class Bindings
+ * Bindings class
  *
- * This class returns classes that will be registered with the theme.
+ * This class is used to register classes that are needed to add features to the theme. classes are grouped according
+ * to their functionlities.
  *
  * @package WordpressThemeStarter
  * @author  Chijindu Nzeako <chijindunzeako517@gmail.com>
@@ -39,103 +42,199 @@ if ( ! defined( 'ABSPATH' ) ) {
 final class Bindings
 {
     /**
-     * Bindings for classes that register admin menus
+     * An array containing classes that will create admin menus.
      *
-     * @static
      * @access public
+     * @static
      * @var array
      * @since 1.0.0
      */
     public static array $menus = array(
+
+        /**
+         * WTSMenuPage::class will create a menu page - "WordPress Theme Starter" in the admin area.
+         *
+         * Remove or Comment out the line below if you do not want to create this menu.
+         */
+
         WTSMenuPage::class => 'admin-menus.wts_menu_page',
+
+        /**
+         * You can add your custom classes below
+         */
+
     );
 
     /**
-     * Bindings for classes that register themes menus
+     * An array containing classes that will create sub menus under `Appearance`.
      *
-     * @static
      * @access public
+     * @static
      * @var array
      * @since 1.0.0
      */
     public static array $themes_menus = array(
+
+        /**
+         * WTSThemePage::class will create a menu page - "WordPress Theme Starter" under `Appearance`.
+         *
+         * Remove or Comment out the line below if you do not want to create this menu.
+         */
+
         WTSThemePage::class => 'admin-menus.wts_theme_page',
+
+        /**
+         * You can add your custom classes below
+         */
+
     );
 
     /**
-     * Bindings for classes that register setting menus
+     * An array containing classes that will create sub menus under `Settings`.
      *
-     * @static
      * @access public
+     * @static
      * @var array
      * @since 1.0.0
      */
     public static array $setting_menus = array(
+
+        /**
+         * WTSOptionsPage::class will create a menu page - "WordPress Theme Starter" under `Settings`.
+         *
+         * Remove or Comment out the line below if you do not want to create this menu.
+         */
+
         WTSOptionsPage::class => 'admin-menus.wts_options_page',
+
+        /**
+         * You can add your custom classes below
+         */
+
     );
 
     /**
-     * Bindings for classes that register sidebars
+     * An array containing classes that will create sidebar areas.
      *
-     * @static
      * @access public
+     * @static
      * @var array
      * @since 1.0.0
      */
     public static array $sidebars = array(
+
+        /**
+         * WTSSidebar::class will register a custom sidebar area - "WordPress Theme Starter Sidebar".
+         *
+         * Remove or Comment out the line below if you do not want to create this sidebar area.
+         */
+
         WTSSidebar::class => 'sidebars.wts_sidebar',
+
+        /**
+         * You can add your custom classes below
+         */
+
     );
 
     /**
-     * Bindings for wordpress widget classes that will be unregistered
+     * An array containing wordpress widget classes that will be unregistered.
      *
-     * @static
      * @access public
+     * @static
      * @var array
      * @since 1.0.0
      */
     public static array $unregistered_widgets = array(
+
+        /**
+         * WP_Widget_Calendar::class will unregister "Calendar" wordpress widget.
+         *
+         * Remove or Comment out the line below if you do not want to unregister this widget.
+         */
+
         WP_Widget_Calendar::class,
+
+        /**
+         * You can add your custom classes below
+         */
+
     );
 
     /**
-     * Bindings for classes that register widgets
+     * An array containing classes that will create widgets.
      *
-     * @static
      * @access public
+     * @static
      * @var array
      * @since 1.0.0
      */
     public static array $widgets = array(
+
+        /**
+         * WTSWidget::class will register a custom wordpress widget - "WordPress Theme Starter Widget".
+         *
+         * Remove or Comment out the line below if you do not want to create this widget.
+         */
+
         WTSWidget::class,
+
+        /**
+         * You can add your custom classes below
+         */
+
     );
 
     /**
-     * Bindings for classes that register customizers
+     * An array containing classes that will create customizer settings, sections and controls.
      *
-     * @static
      * @access public
+     * @static
      * @var array
      * @since 1.0.0
      */
     public static array $customizers = array(
+
+        /**
+         * WTSCustomizer::class will register a custom customizer section with control, visible in the customizer page.
+         *
+         * Remove or Comment out the line below if you do not want to create this customizer settings.
+         */
+
         WTSCustomizer::class => 'customizers.wts_customizer',
+
+        /**
+         * You can add your custom classes below
+         */
+
     );
 
     /**
-     * Bindings for classes that register settings
+     * An array containing classes that will create setting sections and fields.
      *
-     * @static
      * @access public
+     * @static
      * @var array
      * @since 1.0.0
      */
     public static array $settings = array(
+
+        /**
+         * WTSSettings::class will register a custom setting section and field that can be displayed using do_settings_sections().
+         *
+         * Remove or Comment out the line below if you do not want to create this setting.
+         */
+
         WTSSettings::class => 'settings.wts_sidebar_settings',
+
+        /**
+         * You can add your custom classes below
+         */
+
     );
 
     /**
-     * Bindings for classes that create admin notices
+     * An array containing classes that will create admin notices.
      *
      * @access public
      * @static
@@ -145,9 +244,16 @@ final class Bindings
     public static array $admin_notices = array(
 
         /**
-         * Un-comment the line below in order to display a custom notification message on the screen.
+         * WTSNotice::class will display a custom admin notification message on the screen.
+         *
+         * Remove or Comment out the line below if you do not want to create this notification.
          */
+
         WTSNotice::class,
+
+        /**
+         * You can add your custom classes below
+         */
 
     );
 }
